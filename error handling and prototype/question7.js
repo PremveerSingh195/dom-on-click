8e0442e796dd97e80b1b1f8340d9679f86e054b3
PremveerSingh195 <prem.veer2836@gmail.com>
2023-09-22 13:25:49 +0530

    question 7

--- error handling and prototype/question7.js
+++ error handling and prototype/question7.js
@@ -1,6 +1,25 @@
 class User {
     constructor(username , password){
         this.username = username;
-        this.password = password;
+        this.password= password.replace(/./g, '*');
+    }
+    // let regex1 = /\d/;
+    set passset(p){
+         if ( /\d/.test(p) && p.length > 8 && /(\d.*[A-Z])|([A-Z].*\d)/.test(p)) {
+            this.password = p.replace(/./g, '*') ;
+         }else{
+            this.password = " Error: new password is at least 8 characters long and contains at least one number and one uppercase letter."
+         }
+    }
+
+    get passget(){
+        console.log(this.password);
     }
 }
+
+const user1 = new User("Prem", "Alliswell2836");
+console.log(user1);
+
+user1.passset = "Hello1234"
+user1.passget;
+console.log(user1);
